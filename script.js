c141a49d3ca526da9991fee9d832e777bb6c2416
sarina <snnaeimi@gmail.com>
2022-12-23 13:09:06 +0330

    add totalTime and rtl amd x button

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 //get elements
 const timeElement = document.querySelector(".watch .time");
+const totalTimeElement = document.querySelector(".watch .totalTime");
 const startBtn = document.getElementById("startBtn");
 const stopBtn = document.getElementById("stopBtn");
 const resetBtn = document.getElementById("resetBtn");
 
 //initial value
 let seconds = 0;
+let totalTime = 0;
 let interval = null;
 
 //active clock
@@ -52,7 +54,15 @@ function stop() {
   const trackedTime = timeElement.innerText;
 
   const li = document.createElement("li");
+  const deleteBtn = document.createElement("button");
+  deleteBtn.onclick = deleteRow;
+  // var btn = document.createTextNode("\u00D7");
+  deleteBtn.innerText = "x";
+  deleteBtn.className = "closeBtn";
+  // deleteBtn.appendChild(btn);
+
   li.innerText = `${taskName} (${trackedTime})`;
+  li.appendChild(deleteBtn);
 
   const list = document.getElementsByClassName("list")[0];
   list.appendChild(li);
@@ -61,7 +71,13 @@ function stop() {
 //Timer to calculate time
 function timer() {
   seconds++;
+  totalTime++;
 
+  timeElement.innerText = renderTime(seconds);
+  totalTimeElement.innerText = renderTime(totalTime);
+}
+
+function renderTime(seconds) {
   let hrs = Math.floor(seconds / 3600);
   let mins = Math.floor((seconds - hrs * 3600) / 60);
   let secs = seconds % 60;
@@ -70,7 +86,7 @@ function timer() {
   if (mins < 10) mins = "0" + mins;
   if (hrs < 10) hrs = "0" + hrs;
 
-  timeElement.innerText = `${hrs}:${mins}:${secs}`;
+  return `${hrs}:${mins}:${secs}`;
 }
 
 //reset button
@@ -80,4 +96,10 @@ function reset() {
   document.getElementById("txtInput").value = "";
   seconds = 0;
   timeElement.innerText = "00:00:00";
+}
+
+//delete items
+function deleteRow() {
+  var div = this.parentElement;
+  div.style.display = "none";
 }
